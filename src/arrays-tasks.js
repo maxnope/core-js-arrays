604a6f312da707975a8695fec74582157f2e37d2
Evgeniy <e.valerevich@gmail.com>
2025-01-08 21:36:11 +0300

    feat: implement 34 tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -274,7 +274,12 @@ function distinct(arr) {
  *    createNDimensionalArray(1, 1) => [0]
  */
 function createNDimensionalArray(n, size) {
-  return Array(size).fill(Array(n).fill(0));
+  const createArray = (depth) => {
+    return new Array(size).fill(0).map(() => {
+      return depth > 1 ? createArray(depth - 1) : 0;
+    });
+  };
+  return createArray(n);
 }
 
 /**
@@ -288,8 +293,8 @@ function createNDimensionalArray(n, size) {
  *    flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']) => ['a', 'b', 'c', 'd', 'e', 'f']
  *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
  */
-function flattenArray(/* nestedArray */) {
-  throw new Error('Not implemented');
+function flattenArray(nestedArray) {
+  return nestedArray.flat(Infinity);
 }
 
 /**
@@ -305,8 +310,8 @@ function flattenArray(/* nestedArray */) {
  *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
  *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap(childrenSelector);
 }
 
 /**
